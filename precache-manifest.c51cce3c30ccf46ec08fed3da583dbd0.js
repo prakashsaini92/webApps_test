e0ab809ecdc196f7ae2c1ca213359175f978249a
Prakash Saini <Prakash@gisdc.doit>
2019-06-14 16:25:54 +0530

    comments pks

---
+++ precache-manifest.c51cce3c30ccf46ec08fed3da583dbd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ae6099202724ca7229e2236c652f29",
+    "url": "/webApps_test/index.html"
+  },
+  {
+    "revision": "3a871b120d26634e1000",
+    "url": "/webApps_test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/webApps_test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3a871b120d26634e1000",
+    "url": "/webApps_test/static/js/main.6828996a.chunk.js"
+  },
+  {
+    "revision": "efcd948226c421f69f09",
+    "url": "/webApps_test/static/js/runtime~main.b4d237d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webApps_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
